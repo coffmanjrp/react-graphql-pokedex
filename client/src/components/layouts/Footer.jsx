9380bfa634c55eace66a9a2f0718fe454e0092f7
coffmanjrp <coffmanjrp@gmail.com>
2021-03-07 15:39:36 -0800

    Adjust margin/padding

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <div className="flex-container py-6">
+      <div className="flex-container mt-5 py-4">
         <small>
           Pokémon and All Respective Names are Trademark and &copy; of Pokémon
           co./Nintendo/Creatures Inc./GAME FREAK inc. 1995-
