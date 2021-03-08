1c95e4d049c7b41030c9d62f73d932f596da43bf
coffmanjrp <coffmanjrp@gmail.com>
2021-03-07 19:05:35 -0800

    Implement go back link and adjust footer

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <div className="flex-container mt-5 py-4">
+      <div className="footer flex-container  pb-4">
         <small>
           Pokémon and All Respective Names are Trademark and &copy; of Pokémon
           co./Nintendo/Creatures Inc./GAME FREAK inc. 1995-
--- client/src/components/layouts/Header.jsx
+++ client/src/components/layouts/Header.jsx
@@ -1,12 +1,28 @@
+import { useLocation, useHistory } from 'react-router-dom';
 import { CgPokemon } from 'react-icons/cg';
 import { FaChevronLeft } from 'react-icons/fa';
 import { Sidebar } from './';
 
 const Navbar = ({ title }) => {
+  const location = useLocation();
+  const history = useHistory();
+
+  const handleGoBack = () => {
+    history.goBack();
+  };
+
   return (
     <>
       <div className="navbar mb-5">
-        <FaChevronLeft />
+        <div>
+          {location.pathname !== '/' ? (
+            <a href="#!" onClick={handleGoBack}>
+              <FaChevronLeft />
+            </a>
+          ) : (
+            ''
+          )}
+        </div>
         <h1>
           {title}
           <CgPokemon />
